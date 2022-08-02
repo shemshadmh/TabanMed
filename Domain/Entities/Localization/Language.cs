6bbd4836e4a349017a5e4f9ca2ede25623c4d9d1
MHatef <m.hatefshemshad@yahoo.com>
2022-08-02 11:29:52 +0430

    add arabic and dari culture and configurations

---
+++ Domain/Entities/Localization/Language.cs
@@ -0,0 +1,15 @@
+﻿
+using Domain.Entities.Hotels.Translation;
+
+namespace Domain.Entities.Localization
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string IsoName { get; set; } = null!;
+
+        // Hotel Facility Translations
+        public ICollection<HotelFacilityTranslation>? HotelFacilityTranslations { get; set; }
+    }
+}
