b92cfc8fb9893cb346649700ec6e180f2387d258
mhmmdostovari <ostovari.1380@gmail.com>
2022-08-17 17:46:19 +0430

    add MedicalCenter SideBar, MedicalCenterController, Show List Medical caenter by cities

---
+++ Application/Dtos/MedicalCenters/CityWithMedicalCenterCount.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Application.Dtos.MedicalCenters
+{
+    public  class CityWithMedicalCenterCount
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; } = null!;
+        public int MedicalCenterCount { get; set; }
+    }
+}
---
+++ Application/Dtos/MedicalCenters/MedicalCenterListItemDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Resources.DataDictionary;
+
+namespace Application.Dtos.MedicalCenters
+{
+    public class MedicalCenterListItemDto
+    {
+        public int Id { get; set; }
+        [Display(ResourceType = typeof(DataDictionary),
+            Name = nameof(DataDictionary.Name))]
+        
+        public string Name { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string? AgentPhoneNumber { get; set; }
+        public string ImageUrl { get; set; } = null!;
+    }
+}
